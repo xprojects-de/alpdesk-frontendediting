f4ee43e29ce81352b6d4e898c3fac6379582b898
XProjects B.Hummel <infos@x-projects.de>
2020-12-19 00:34:44 +0100

    Bugfix NewMapping

--- src/Mapping/Mappingtypes/TypeNewslist.php
+++ src/Mapping/Mappingtypes/TypeNewslist.php
@@ -15,10 +15,10 @@ class TypeNewslist extends Base {
 
     // @TODO very hard because there could be more news_teasers!! No Idea at the Moment when more news are selected
     // At the Moment link to global News if more News are selected
-    if (class_exists('\Contao\NewsModel')) {
+    if (class_exists('\Contao\NewsArchiveModel')) {
       $newsarchives = StringUtil::deserialize($this->module->news_archives);
       if (count($newsarchives) == 1) {
-        $objNews = \Contao\NewsModel::findById($newsarchives[0]);
+        $objNews = \Contao\NewsArchiveModel::findById($newsarchives[0]);
         if ($objNews !== null) {
           if (BackendUser::getInstance()->hasAccess($objNews->id, 'news')) {
             $item->setValid(true);
