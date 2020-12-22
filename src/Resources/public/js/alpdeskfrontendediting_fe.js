4cbb6d63e04f19c5f71de2aa8550e8ba681f0a7d
XProjects B.Hummel <infos@x-projects.de>
2020-12-22 13:35:15 +0100

    Bugfix bar-position relative to Contao-Bar

--- src/Resources/public/js/alpdeskfrontendediting_fe.js
+++ src/Resources/public/js/alpdeskfrontendediting_fe.js
@@ -365,7 +365,7 @@
       if (showPageEdit === true && alpdeskfeePageid !== null && alpdeskfeePageid !== '' && alpdeskfeePageid !== undefined && alpdeskfeePageid !== 0) {
         const bodyElement = document.body;
         if (bodyElement !== null && bodyElement !== undefined) {
-          const jsonData = '{"type":"' + TARGETTYPE_PAGE + '","desc":"Page","do":"' + TARGETTYPE_PAGE + '","id":"' + alpdeskfeePageid + '","pageid":"' + alpdeskfeePageid + '"}';
+          const jsonData = '{"type":"' + TARGETTYPE_PAGE + '","do":"' + TARGETTYPE_PAGE + '","id":"' + alpdeskfeePageid + '","pageid":"' + alpdeskfeePageid + '"}';
           const obj = JSON.parse(jsonData);
           if (obj !== null && obj !== undefined) {
             appendUtilsContainer(obj, bodyElement, true, objLabels);
