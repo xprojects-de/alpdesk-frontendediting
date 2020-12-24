38112014db3570db493cf2b24693880a4193e4fb
XProjects B.Hummel <infos@x-projects.de>
2020-12-24 12:13:50 +0100

    RockSolidSlider as Module

--- src/Mapping/Mappingtypes/TypeRockSolidSlider.php
+++ src/Mapping/Mappingtypes/TypeRockSolidSlider.php
@@ -15,6 +15,17 @@ class TypeRockSolidSlider extends Base {
 
   public function run(CustomViewItem $item): CustomViewItem {
 
+    if ($this->element !== null) {
+      $this->parseElement($item);
+    } else if ($this->module !== null) {
+      $this->parseModule($item);
+    }
+    
+    return $item;
+  }
+
+  private function parseElement(CustomViewItem $item): void {
+
     $do = self::$DO . '&act=edit&id=' . $this->element->rsts_id;
 
     if (class_exists('\MadeYourDay\RockSolidSlider\Model\SliderModel')) {
@@ -31,8 +42,27 @@ class TypeRockSolidSlider extends Base {
     $item->setIconclass(self::$iconclass);
     $item->setPath($do);
     $item->setLabel($GLOBALS['TL_LANG']['alpdeskfee_mapping_lables']['rocksolidslider']);
+    
+  }
 
-    return $item;
+  private function parseModule(CustomViewItem $item): void {
+
+    $do = self::$DO . '&act=edit&id=' . $this->module->rsts_id;
+
+    if (class_exists('\MadeYourDay\RockSolidSlider\Model\SliderModel')) {
+      $sliderModel = \MadeYourDay\RockSolidSlider\Model\SliderModel::findById($this->module->rsts_id);
+      if ($sliderModel !== null) {
+        if ($sliderModel->type == 'content') {
+          $do = self::$DO . '&table=tl_rocksolid_slide&id=' . $this->module->rsts_id;
+        }
+      }
+    }
+
+    $item->setValid(true);
+    $item->setIcon(self::$icon);
+    $item->setIconclass(self::$iconclass);
+    $item->setPath($do);
+    $item->setLabel($GLOBALS['TL_LANG']['alpdeskfee_mapping_lables']['rocksolidslider']);
   }
 
 }
