d1f404542a9b95b529dde172a5fa46423b3d515e
XProjects B.Hummel <infos@x-projects.de>
2020-12-25 21:36:40 +0100

    Do not use key from config.yml for Mapping

--- src/Mapping/Mappingtypes/Base.php
+++ src/Mapping/Mappingtypes/Base.php
@@ -31,22 +31,27 @@ abstract class Base {
   public static function findClassByElement(ContentModel $element, array $mappingconfig) {
 
     if ($mappingconfig !== null && \is_array($mappingconfig)) {
-      if (\array_key_exists($element->type, $mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'])) {
-        $backendmodule = $mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'][$element->type]['backend_module'];
-        $mappingObject = $mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'][$element->type]['mapping_object'];
-        $icon = $mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'][$element->type]['icon'];
-        $iconclass = $mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'][$element->type]['iconclass'];
-        $labelkey = $mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'][$element->type]['labelkey'];
-        $additional_static_params = $mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'][$element->type]['additional_static_params'];
-        if (self::checkModuleAccess($backendmodule)) {
-          $class = new $mappingObject();
-          $class->element = $element;
-          $class->icon = $icon;
-          $class->iconclass = $iconclass;
-          $class->label = $GLOBALS['TL_LANG']['alpdeskfee_mapping_lables'][$labelkey];
-          $class->backendmodule = $backendmodule;
-          $class->additional_static_params = $additional_static_params;
-          return $class;
+      foreach ($mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'] as $value) {
+        if ($value['element'] !== null) {
+          if ($element->type == $value['element']) {
+            $backendmodule = $value['backend_module'];
+            $mappingObject = $value['mapping_object'];
+            $icon = $value['icon'];
+            $iconclass = $value['iconclass'];
+            $labelkey = $value['labelkey'];
+            $additional_static_params = $value['additional_static_params'];
+            if (self::checkModuleAccess($backendmodule)) {
+              $class = new $mappingObject();
+              $class->element = $element;
+              $class->icon = $icon;
+              $class->iconclass = $iconclass;
+              $class->label = $GLOBALS['TL_LANG']['alpdeskfee_mapping_lables'][$labelkey];
+              $class->backendmodule = $backendmodule;
+              $class->additional_static_params = $additional_static_params;
+              return $class;
+            }
+            break;
+          }
         }
       }
     }
@@ -57,8 +62,8 @@ abstract class Base {
   public static function findClassByModule(Module $module, array $mappingconfig) {
 
     if ($mappingconfig !== null && \is_array($mappingconfig)) {
-      foreach ($mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'] as $key => $value) {
-        if ($value['is_module'] == true) {
+      foreach ($mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'] as $value) {
+        if ($value['module'] == true) {
           $mappingObject = $value['mapping_object'];
           $backendmodule = $value['backend_module'];
           $moduleobject = $value['module_object'];
