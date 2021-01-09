ece51368a035430cd49c5f4ba290b911715b9f3f
XProjects B.Hummel <infos@x-projects.de>
2021-01-09 20:46:14 +0100

    First DragAndDrop

--- alpdeskfee-client/src/app/directives/item-bar.directive.ts
+++ alpdeskfee-client/src/app/directives/item-bar.directive.ts
@@ -1,15 +1,20 @@
-import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input, OnDestroy } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: '[appItemBar]'
 })
-export class ItemBarDirective implements AfterViewInit {
+export class ItemBarDirective implements AfterViewInit, OnDestroy {
 
   @Input() frameContentDocument!: HTMLDocument;
   @Input() selectedElement!: HTMLElement;
+  @Input() sticky: boolean = false;
 
   private element: HTMLElement;
   private isSticky: boolean = false;
+
+  private subscriptions: Subscription[] = [];
 
   constructor(el: ElementRef) {
     this.element = el.nativeElement;
@@ -17,6 +22,12 @@ export class ItemBarDirective implements AfterViewInit {
 
   ngAfterViewInit() {
     //this.stickyBarItem();
+    this.draggableElement();
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
   }
 
   private stickyBarItem() {
@@ -39,4 +50,44 @@ export class ItemBarDirective implements AfterViewInit {
     }
   }
 
+  draggableElement() {
+
+    let moveItem = this.element.querySelector('app-item-move') as HTMLElement;
+
+    if (moveItem !== null && moveItem !== undefined) {
+
+      const dragStart$ = fromEvent<MouseEvent>(moveItem, "mousedown");
+      const dragEnd$ = fromEvent<MouseEvent>(this.frameContentDocument, "mouseup");
+      const drag$ = fromEvent<MouseEvent>(this.frameContentDocument, "mousemove").pipe(
+        takeUntil(dragEnd$)
+      );
+
+      let initialX: number, initialY: number, currentX = 0, currentY = 0;
+      let dragSub!: Subscription;
+
+      const dragStartSub = dragStart$.subscribe((event: MouseEvent) => {
+        initialX = event.clientX - currentX;
+        initialY = event.clientY - currentY;
+
+        dragSub = drag$.subscribe((event: MouseEvent) => {
+          event.preventDefault();
+          currentX = event.clientX - initialX;
+          currentY = event.clientY - initialY;
+          this.element.style.top = currentY + "px";
+          this.element.style.left = currentX + "px";
+          //this.element.style.transform = "translate3d(" + currentX + "px, " + currentY + "px, 0)";
+        });
+      });
+
+      const dragEndSub = dragEnd$.subscribe(() => {
+        initialX = currentX;
+        initialY = currentY;
+        if (dragSub) {
+          dragSub.unsubscribe();
+        }
+      });
+
+      this.subscriptions.push.apply(this.subscriptions, [dragStartSub, dragSub, dragEndSub,]);
+    }
+  }
 }
--- alpdeskfee-client/src/app/items/item-move/item-move.component.ts
+++ alpdeskfee-client/src/app/items/item-move/item-move.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, HostListener, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { BaseItemComponent } from '../base-item/base-item.component';
 
 @Component({
@@ -6,95 +6,6 @@ import { BaseItemComponent } from '../base-item/base-item.component';
   templateUrl: './item-move.component.html',
   styleUrls: ['./item-move.component.scss']
 })
-export class ItemMoveComponent extends BaseItemComponent implements OnInit, AfterViewInit, OnDestroy {
+export class ItemMoveComponent extends BaseItemComponent {
 
-  @Input() containerElement!: any;
-
-  @ViewChild('moveItem') moveItem!: ElementRef;
-
-  ngOnInit() {
-
-  }
-
-  ngAfterViewInit() {
-    if (this.containerElement !== null && this.containerElement !== undefined && this.moveItem !== null && this.moveItem !== undefined) {
-      //this.draggableElement();
-    }
-  }
-
-  ngOnDestroy() {
-    //this.moveItem.nativeElement.removeEventListener('mousedown');
-    //this.containerElement.removeEventListener('onmouseup');
-    //this.containerElement.removeEventListener('onmousemove');
-    //console.log("Removed event listener");
-  }
-
-  draggableElement() {
-
-    let initialX: number;
-    let initialY: number;
-
-    let element = this.moveItem.nativeElement;
-    let containerElement = this.containerElement;
-
-    let barContainer!: any;
-    let currentElement!: any;
-
-    // This element is unique and there is no check needed
-    element.onmousedown = function (e: any) {
-      e = e || window.event;
-      initialX = e.clientX;
-      initialY = e.clientY;
-      if (e.target === this) {
-        currentElement = this;
-        barContainer = this.parentElement.parentElement;
-      }
-    };
-
-    containerElement.onmouseup = function () {
-      currentElement = null;
-      barContainer = null;
-    };
-
-    containerElement.onmousemove = function (e: any) {
-      if (currentElement !== null && currentElement !== undefined && barContainer !== null && barContainer !== undefined) {
-        //console.log(barContainer);
-        e = e || window.event;
-        e.preventDefault();
-        let currentX = initialX - e.clientX;
-        let currentY = initialY - e.clientY;
-        initialX = e.clientX;
-        initialY = e.clientY;
-
-        let limit_bottom = containerElement.offsetHeight - currentY - barContainer.offsetHeight;
-        let top = (barContainer.offsetTop - currentY);
-        if (barContainer.offsetTop >= limit_bottom) {
-          top = limit_bottom;
-        } else if (top <= 0) {
-          top = 0;
-        }
-
-        let limit_left = containerElement.offsetWidth - currentX - barContainer.offsetWidth;
-        let left = (barContainer.offsetLeft - currentX);
-        if (barContainer.offsetLeft >= limit_left) {
-          left = limit_left;
-        }
-        // Bar is left not in the middle
-        if (barContainer.classList.contains('alpdeskfee-utilscontainer-custommodule')) {
-          if (left <= 0) {
-            left = 0;
-          }
-        } else {
-          if (left <= (barContainer.offsetWidth / 2)) {
-            left = (barContainer.offsetWidth / 2);
-          }
-        }
-
-        barContainer.style.top = top + "px";
-        barContainer.style.left = left + "px";
-        //console.log(top);
-        //console.log(left);
-      }
-    };
-  }
 }
