e34a9b92b1b2caae182f6b217757e887e35277fa
XProjects B.Hummel <infos@x-projects.de>
2021-02-04 18:13:49 +0100

    adding FormEvent

---
+++ src/Events/AlpdeskFrontendeditingEventForm.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alpdesk\AlpdeskFrontendediting\Events;
+
+use Symfony\Contracts\EventDispatcher\Event;
+use Alpdesk\AlpdeskFrontendediting\Custom\CustomViewItem;
+use Contao\Form;
+
+class AlpdeskFrontendeditingEventForm extends Event {
+
+  public const NAME = 'alpdeskfrontendediting.form';
+
+  private $item;
+  private $form;
+
+  public function __construct(CustomViewItem $item, Form $form) {
+    $this->item = $item;
+    $this->form = $form;
+  }
+
+  public function getItem(): CustomViewItem {
+    return $this->item;
+  }
+
+  public function setItem(CustomViewItem $item): void {
+    $this->item = $item;
+  }
+
+  public function getForm(): Form {
+    return $this->form;
+  }
+
+  public function setForm(Form $form): void {
+    $this->form = $form;
+  }
+
+}
