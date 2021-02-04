0b7520a5b574f3fea9f81641e6f0428ba4c8b2e2
XProjects B.Hummel <infos@x-projects.de>
2021-02-04 18:15:52 +0100

    adding findClassByForm

--- src/Mapping/Mappingtypes/Base.php
+++ src/Mapping/Mappingtypes/Base.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Alpdesk\AlpdeskFrontendediting\Mapping\Mappingtypes;
 
 use Contao\Module;
+use Contao\Form;
 use Contao\ContentModel;
 use Contao\BackendUser;
 use Alpdesk\AlpdeskFrontendediting\Custom\CustomViewItem;
@@ -15,6 +16,7 @@ abstract class Base {
 
   public $module;
   public $element;
+  public $form;
   public $icon;
   public $iconclass;
   public $label;
@@ -79,6 +81,40 @@ abstract class Base {
             if (self::checkModuleAccess($backendmodule)) {
               $class = new $mappingObject();
               $class->module = $module;
+              $class->icon = $icon;
+              $class->iconclass = $iconclass;
+              $class->label = $GLOBALS['TL_LANG']['alpdeskfee_mapping_lables'][$labelkey];
+              $class->backendmodule = $backendmodule;
+              $class->additional_static_params = $additional_static_params;
+              $class->table = $table;
+              return $class;
+            }
+            break;
+          }
+        }
+      }
+    }
+
+    return null;
+  }
+
+  public static function findClassByForm(Form $form, array $mappingconfig) {
+
+    if ($mappingconfig !== null && \is_array($mappingconfig)) {
+      foreach ($mappingconfig['alpdesk_frontendediting_mapping']['type_mapping'] as $value) {
+        if ($value['module'] !== null) {
+          $mappingObject = $value['mapping_object'];
+          $backendmodule = $value['backend_module'];
+          $moduleobject = $value['module'];
+          $icon = $value['icon'];
+          $iconclass = $value['iconclass'];
+          $labelkey = $value['labelkey'];
+          $additional_static_params = $value['additional_static_params'];
+          $table = $value['table'];
+          if (class_exists($moduleobject) && $form instanceof $moduleobject) {
+            if (self::checkModuleAccess($backendmodule)) {
+              $class = new $mappingObject();
+              $class->form = $form;
               $class->icon = $icon;
               $class->iconclass = $iconclass;
               $class->label = $GLOBALS['TL_LANG']['alpdeskfee_mapping_lables'][$labelkey];
